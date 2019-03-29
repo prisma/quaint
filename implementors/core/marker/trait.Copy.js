8c0ad996cd39e0961bdf427d45969b0fc012a0b0
Buildkite agent <hello@prisma.io>
2019-03-29 12:34:25 +0000

    Rebuild pages at dbec3bab68b4ef7324a517a3417e6b43c26740e2

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"prisma_query/ast/enum.Order.html\" title=\"enum prisma_query::ast::Order\">Order</a>",synthetic:false,types:["prisma_query::ast::ordering::Order"]},];
+implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"prisma_query/ast/enum.OnConflict.html\" title=\"enum prisma_query::ast::OnConflict\">OnConflict</a>",synthetic:false,types:["prisma_query::ast::insert::OnConflict"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"prisma_query/ast/enum.Order.html\" title=\"enum prisma_query::ast::Order\">Order</a>",synthetic:false,types:["prisma_query::ast::ordering::Order"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
