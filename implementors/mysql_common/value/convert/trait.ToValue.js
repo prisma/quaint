1db8b48a9dfe2ec5b8c5b4d9269e9bdf572ebced
Buildkite agent <hello@prisma.io>
2019-05-16 12:32:09 +0000

    Rebuild pages at 5e9839c7719f2d4a14dd29c1d48bff024a96dbcb

---
+++ implementors/mysql_common/value/convert/trait.ToValue.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["prisma_query"] = [{text:"impl ToValue for <a class=\"enum\" href=\"prisma_query/ast/enum.ParameterizedValue.html\" title=\"enum prisma_query::ast::ParameterizedValue\">ParameterizedValue</a>",synthetic:false,types:["prisma_query::ast::values::ParameterizedValue"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
