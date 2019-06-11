90311f3376054d0a48eb32f841f484ebbb4f7468
Buildkite agent <hello@prisma.io>
2019-06-11 14:46:23 +0000

    Rebuild pages at a75f3ad6715befce99efe58d46b34aa700a333dd

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["prisma_query"] = [{text:"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;'_ <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"prisma_query/connector/struct.Sqlite.html\" title=\"struct prisma_query::connector::Sqlite\">Sqlite</a>",synthetic:false,types:["prisma_query::connector::sqlite::Sqlite"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
