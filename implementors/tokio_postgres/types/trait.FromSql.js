574a27ad8135e9d2bcac715cf07716163a0efe2e
Buildkite agent <hello@prisma.io>
2019-07-01 14:52:15 +0000

    Rebuild pages at e34b0162c17d21286b86c7fc1b654048afe6a66a

--- implementors/tokio_postgres/types/trait.FromSql.js
+++ implementors/tokio_postgres/types/trait.FromSql.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/tokio-postgres/0.4.0-rc.1/tokio_postgres/types/trait.FromSql.html\" title=\"trait tokio_postgres::types::FromSql\">FromSql</a>&lt;'a&gt; for <a class=\"enum\" href=\"prisma_query/ast/enum.Id.html\" title=\"enum prisma_query::ast::Id\">Id</a>",synthetic:false,types:["prisma_query::ast::id::Id"]},];
+implementors["prisma_query"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/tokio-postgres/0.4.0-rc.3/tokio_postgres/types/trait.FromSql.html\" title=\"trait tokio_postgres::types::FromSql\">FromSql</a>&lt;'a&gt; for <a class=\"enum\" href=\"prisma_query/ast/enum.Id.html\" title=\"enum prisma_query::ast::Id\">Id</a>",synthetic:false,types:["prisma_query::ast::id::Id"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/tokio_postgres/types/trait.ToSql.js
+++ implementors/tokio_postgres/types/trait.ToSql.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-postgres/0.4.0-rc.1/tokio_postgres/types/trait.ToSql.html\" title=\"trait tokio_postgres::types::ToSql\">ToSql</a> for <a class=\"enum\" href=\"prisma_query/ast/enum.ParameterizedValue.html\" title=\"enum prisma_query::ast::ParameterizedValue\">ParameterizedValue</a>",synthetic:false,types:["prisma_query::ast::values::ParameterizedValue"]},];
+implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/tokio-postgres/0.4.0-rc.3/tokio_postgres/types/trait.ToSql.html\" title=\"trait tokio_postgres::types::ToSql\">ToSql</a> for <a class=\"enum\" href=\"prisma_query/ast/enum.ParameterizedValue.html\" title=\"enum prisma_query::ast::ParameterizedValue\">ParameterizedValue</a>",synthetic:false,types:["prisma_query::ast::values::ParameterizedValue"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
