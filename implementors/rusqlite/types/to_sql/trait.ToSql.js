a84bc411103fd7dcd9cbb72fc3cf32aa3c7423c8
Buildkite agent <hello@prisma.io>
2019-07-01 16:07:29 +0000

    Rebuild pages at 2e4adf03f516ced55496dbfebbfc810ea0db1aa4

--- implementors/rusqlite/types/to_sql/trait.ToSql.js
+++ implementors/rusqlite/types/to_sql/trait.ToSql.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl ToSql for <a class=\"enum\" href=\"prisma_query/ast/enum.ParameterizedValue.html\" title=\"enum prisma_query::ast::ParameterizedValue\">ParameterizedValue</a>",synthetic:false,types:["prisma_query::ast::values::ParameterizedValue"]},];
+implementors["prisma_query"] = [{text:"impl&lt;'a&gt; ToSql for <a class=\"enum\" href=\"prisma_query/ast/enum.ParameterizedValue.html\" title=\"enum prisma_query::ast::ParameterizedValue\">ParameterizedValue</a>&lt;'a&gt;",synthetic:false,types:["prisma_query::ast::values::ParameterizedValue"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
