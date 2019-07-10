6d81a84549f518bcf12bcdb8a56a06e06850de69
Buildkite agent <hello@prisma.io>
2019-07-10 17:05:43 +0000

    Rebuild pages at 8a9db9f6b4bef0a2f76a323e2487c8f78e4b4822

--- implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"prisma_query/struct.ResultSet.html\" title=\"struct prisma_query::ResultSet\">ResultSet</a>",synthetic:false,types:["prisma_query::result_set::ResultSet"]},];
+implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"prisma_query/connector/struct.ResultSet.html\" title=\"struct prisma_query::connector::ResultSet\">ResultSet</a>",synthetic:false,types:["prisma_query::connector::result_set::ResultSet"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
