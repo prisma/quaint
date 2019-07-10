1a947aae3a4ed26d94d3459d2f2efb74b51629ec
Buildkite agent <hello@prisma.io>
2019-07-10 13:22:08 +0000

    Rebuild pages at 5e8f3a84c54ba8450d3acf8c1ecf3d8bafc6eeb8

--- implementors/core/iter/traits/iterator/trait.Iterator.js
+++ implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"prisma_query/struct.ResultSetIterator.html\" title=\"struct prisma_query::ResultSetIterator\">ResultSetIterator</a>&lt;'a&gt;",synthetic:false,types:["prisma_query::result_set::ResultSetIterator"]},];
+implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"prisma_query/struct.ResultSetIterator.html\" title=\"struct prisma_query::ResultSetIterator\">ResultSetIterator</a>",synthetic:false,types:["prisma_query::result_set::ResultSetIterator"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/ops/index/trait.Index.js
+++ implementors/core/ops/index/trait.Index.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"prisma_query/struct.ResultRowWithName.html\" title=\"struct prisma_query::ResultRowWithName\">ResultRowWithName</a>&lt;'a&gt;",synthetic:false,types:["prisma_query::result_set::ResultRowWithName"]},];
+implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"prisma_query/struct.ResultRowWithName.html\" title=\"struct prisma_query::ResultRowWithName\">ResultRowWithName</a>",synthetic:false,types:["prisma_query::result_set::ResultRowWithName"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
