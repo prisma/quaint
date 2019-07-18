8521b79da2124414a89d5480f6ff1c66036735f7
Buildkite agent <hello@prisma.io>
2019-07-18 13:35:36 +0000

    Rebuild pages at 39f3b609b05a29e2abd37d0608b11f2b30bd4805

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["prisma_query"] = [{text:"impl&lt;'a, Q&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"prisma_query/connector/struct.Transaction.html\" title=\"struct prisma_query::connector::Transaction\">Transaction</a>&lt;'a, Q&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Q: <a class=\"trait\" href=\"prisma_query/connector/trait.Queryable.html\" title=\"trait prisma_query::connector::Queryable\">Queryable</a>,&nbsp;</span>",synthetic:false,types:["prisma_query::connector::transaction::Transaction"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
