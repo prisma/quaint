7aef0341834422e55108dcff72c46b8c0e7e0694
Buildkite agent <hello@prisma.io>
2019-07-31 16:45:17 +0000

    Rebuild pages at 43e55ad3294ce165bda009d58b4a650704a42aeb

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["prisma_query"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"prisma_query/error/enum.Error.html\" title=\"enum prisma_query::error::Error\">Error</a>",synthetic:false,types:["prisma_query::error::Error"]},];
+implementors["prisma_query"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"prisma_query/ast/enum.ParameterizedValue.html\" title=\"enum prisma_query::ast::ParameterizedValue\">ParameterizedValue</a>&lt;'a&gt;",synthetic:false,types:["prisma_query::ast::values::ParameterizedValue"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"prisma_query/error/enum.Error.html\" title=\"enum prisma_query::error::Error\">Error</a>",synthetic:false,types:["prisma_query::error::Error"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
