4332709d6291b003fdf454525506acd4ec2c27d0
Julius de Bruijn <bruijn@prisma.io>
2019-11-13 15:01:26 +0100

    Manual docs commit

--- implementors/quaint/pool/trait.Manage.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["quaint"] = [{text:"impl <a class=\"trait\" href=\"quaint/pool/trait.Manage.html\" title=\"trait quaint::pool::Manage\">Manage</a> for <a class=\"struct\" href=\"quaint/pool/struct.MysqlManager.html\" title=\"struct quaint::pool::MysqlManager\">MysqlManager</a>",synthetic:false,types:["quaint::pool::mysql::MysqlManager"]},{text:"impl <a class=\"trait\" href=\"quaint/pool/trait.Manage.html\" title=\"trait quaint::pool::Manage\">Manage</a> for <a class=\"struct\" href=\"quaint/pool/struct.PostgresManager.html\" title=\"struct quaint::pool::PostgresManager\">PostgresManager</a>",synthetic:false,types:["quaint::pool::postgres::PostgresManager"]},{text:"impl <a class=\"trait\" href=\"quaint/pool/trait.Manage.html\" title=\"trait quaint::pool::Manage\">Manage</a> for <a class=\"struct\" href=\"quaint/pool/struct.SqliteManager.html\" title=\"struct quaint::pool::SqliteManager\">SqliteManager</a>",synthetic:false,types:["quaint::pool::sqlite::SqliteManager"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
