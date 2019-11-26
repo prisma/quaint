f34d13d498f84407aeeda8628caf902a1e38674d
Buildkite agent <hello@prisma.io>
2019-11-26 17:43:51 +0000

    Rebuild pages at 062d394e81d8bc6ff9eba57567b9b4fccd73ce29

--- quaint/pooled/sidebar-items.js
+++ quaint/pooled/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Quaint","The main entry point and an abstraction over database connections and"]]});
+initSidebarItems({"struct":[["PooledConnection","A connection from the pool. Implements Queryable."],["Quaint","The main entry point and an abstraction over database connections and"]]});
