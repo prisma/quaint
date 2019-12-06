c4dab43d60a73f3b27e83038e2ccb6df1a965553
Buildkite agent <hello@prisma.io>
2019-12-06 14:47:15 +0000

    [skip ci] Rebuild pages at 78c7ce5ab0178622e4da00a092edc6c3012e890e

---
+++ implementors/core/ops/arith/trait.Rem.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["quaint"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a>&lt;<a class=\"enum\" href=\"quaint/ast/enum.DatabaseValue.html\" title=\"enum quaint::ast::DatabaseValue\">DatabaseValue</a>&lt;'a&gt;&gt; for <a class=\"enum\" href=\"quaint/ast/enum.DatabaseValue.html\" title=\"enum quaint::ast::DatabaseValue\">DatabaseValue</a>&lt;'a&gt;",synthetic:false,types:["quaint::ast::values::DatabaseValue"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
