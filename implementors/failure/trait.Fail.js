f28eb463a6548d3a5c6df9bcf6ed4937000859c9
Buildkite agent <hello@prisma.io>
2019-12-27 20:58:30 +0000

    [skip ci] Rebuild pages at 6bcd91054ea0b141e90ebefe9d819569829b46a6

--- implementors/failure/trait.Fail.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["quaint"] = [{text:"impl Fail for <a class=\"enum\" href=\"quaint/error/enum.Error.html\" title=\"enum quaint::error::Error\">Error</a>",synthetic:false,types:["quaint::error::Error"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
