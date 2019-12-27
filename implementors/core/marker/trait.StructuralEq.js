f0ae7bd39f6658ae604a191bb2093ff1b7674027
Buildkite agent <hello@prisma.io>
2019-12-27 20:02:00 +0000

    [skip ci] Rebuild pages at 86a180e91fed86676b58dd9c82d6656656535414

---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["quaint"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"quaint/ast/enum.Id.html\" title=\"enum quaint::ast::Id\">Id</a>",synthetic:false,types:["quaint::ast::id::Id"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"quaint/ast/enum.Order.html\" title=\"enum quaint::ast::Order\">Order</a>",synthetic:false,types:["quaint::ast::ordering::Order"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
