dc333379d87b1ec91bb7b6a67cd0f2ce52a9153c
Buildkite agent <hello@prisma.io>
2020-01-19 12:52:12 +0000

    [skip ci] Rebuild pages at 4bbdf1d79afe245848f22efeddb51465ad8357ed

---
+++ quaint/serde/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["from_row","Deserialize a row into any type implementing `Deserialize`."],["from_rows","Deserialize each row of a `ResultSet`."]],"struct":[["FromRowError",""],["ParameterizedValueDeserializer",""]]});
