e7970f0645f017dcb929c6c439b30a4d75a7d202
Buildkite agent <hello@prisma.io>
2020-01-27 13:55:50 +0000

    [skip ci] Rebuild pages at 66fc09ad7c6787579a59e23426aa4aa82a473375

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["quaint"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"quaint/error/enum.Error.html\" title=\"enum quaint::error::Error\">Error</a>",synthetic:false,types:["quaint::error::Error"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"quaint/serde/struct.FromRowError.html\" title=\"struct quaint::serde::FromRowError\">FromRowError</a>",synthetic:false,types:["quaint::serde::FromRowError"]},];
+implementors["quaint"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"quaint/error/enum.Error.html\" title=\"enum quaint::error::Error\">Error</a>",synthetic:false,types:["quaint::error::Error"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
