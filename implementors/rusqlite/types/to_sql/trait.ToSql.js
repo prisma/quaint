bcda788093b8bf19dc14fd7f29ea1ad97c3e7aa2
Buildkite agent <hello@prisma.io>
2020-04-28 14:05:15 +0000

    [skip ci] Rebuild pages at 24ba5bfea110d08a2c0ee8b54daa2fcb4639a292

--- implementors/rusqlite/types/to_sql/trait.ToSql.js
+++ implementors/rusqlite/types/to_sql/trait.ToSql.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["quaint"] = [{"text":"impl&lt;'a&gt; ToSql for <a class=\"enum\" href=\"quaint/ast/enum.ParameterizedValue.html\" title=\"enum quaint::ast::ParameterizedValue\">ParameterizedValue</a>&lt;'a&gt;","synthetic":false,"types":["quaint::ast::values::ParameterizedValue"]}];
+implementors["quaint"] = [{"text":"impl&lt;'a&gt; ToSql for <a class=\"enum\" href=\"quaint/enum.Value.html\" title=\"enum quaint::Value\">Value</a>&lt;'a&gt;","synthetic":false,"types":["quaint::ast::values::Value"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
