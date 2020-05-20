490ff9c3acee0ac4a70da699b3f1011d62671a65
Buildkite agent <hello@prisma.io>
2020-05-20 15:55:56 +0000

    [skip ci] Rebuild pages at 4460975d1c81b853a270c378dbae6cd6e25b2e09

--- implementors/core/future/future/trait.Future.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["quaint"] = [{"text":"impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"quaint/connector/struct.DBIO.html\" title=\"struct quaint::connector::DBIO\">DBIO</a>&lt;'a, T&gt;","synthetic":false,"types":["quaint::connector::dbio::DBIO"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
