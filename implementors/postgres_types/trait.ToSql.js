ed56a0a52654c5f4c3e7a2f4467bb89b587d6bbf
pimeys <pimeys@users.noreply.github.com>
2021-04-07 14:40:07 +0000

    Automated deployment: Wed Apr  7 14:40:07 UTC 2021 8c4544130e1fe1cbc0f95ec3b21e616763774808

--- implementors/postgres_types/trait.ToSql.js
+++ implementors/postgres_types/trait.ToSql.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["quaint"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/postgres-types/0.1/postgres_types/trait.ToSql.html\" title=\"trait postgres_types::ToSql\">ToSql</a> for <a class=\"enum\" href=\"quaint/ast/enum.Value.html\" title=\"enum quaint::ast::Value\">Value</a>&lt;'a&gt;","synthetic":false,"types":["quaint::ast::values::Value"]}];
+implementors["quaint"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/postgres-types/0.2/postgres_types/trait.ToSql.html\" title=\"trait postgres_types::ToSql\">ToSql</a> for <a class=\"enum\" href=\"quaint/ast/enum.Value.html\" title=\"enum quaint::ast::Value\">Value</a>&lt;'a&gt;","synthetic":false,"types":["quaint::ast::values::Value"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
