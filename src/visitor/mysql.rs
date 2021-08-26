e89032440d36808974e02362d10a240ad56aaebf
Dominic Petrick <dompetrick@gmail.com>
2021-08-26 15:04:43 +0200

    Fix json negation (not_eq)

--- src/visitor/mysql.rs
+++ src/visitor/mysql.rs
@@ -245,20 +245,22 @@ impl<'a> Visitor<'a> for Mysql<'a> {
         #[cfg(feature = "json")]
         {
             if right.is_json_value() || left.is_json_value() {
-                self.write("JSON_CONTAINS")?;
-                self.surround_with("(", ")", |s| {
-                    s.visit_expression(left.clone())?;
-                    s.write(", ")?;
-                    s.visit_expression(right.clone())
-                })?;
+                self.surround_with("(", ")", |ref mut s| {
+                    s.write("JSON_CONTAINS")?;
+                    s.surround_with("(", ")", |s| {
+                        s.visit_expression(left.clone())?;
+                        s.write(", ")?;
+                        s.visit_expression(right.clone())
+                    })?;
 
-                self.write(" AND ")?;
+                    s.write(" AND ")?;
 
-                self.write("JSON_CONTAINS")?;
-                self.surround_with("(", ")", |s| {
-                    s.visit_expression(right)?;
-                    s.write(", ")?;
-                    s.visit_expression(left)
+                    s.write("JSON_CONTAINS")?;
+                    s.surround_with("(", ")", |s| {
+                        s.visit_expression(right)?;
+                        s.write(", ")?;
+                        s.visit_expression(left)
+                    })
                 })
             } else {
                 self.visit_regular_equality_comparison(left, right)
@@ -275,20 +277,22 @@ impl<'a> Visitor<'a> for Mysql<'a> {
         #[cfg(feature = "json")]
         {
             if right.is_json_value() || left.is_json_value() {
-                self.write("NOT JSON_CONTAINS")?;
-                self.surround_with("(", ")", |s| {
-                    s.visit_expression(left.clone())?;
-                    s.write(", ")?;
-                    s.visit_expression(right.clone())
-                })?;
+                self.surround_with("(", ")", |ref mut s| {
+                    s.write("NOT JSON_CONTAINS")?;
+                    s.surround_with("(", ")", |s| {
+                        s.visit_expression(left.clone())?;
+                        s.write(", ")?;
+                        s.visit_expression(right.clone())
+                    })?;
 
-                self.write(" OR ")?;
+                    s.write(" OR ")?;
 
-                self.write("NOT JSON_CONTAINS")?;
-                self.surround_with("(", ")", |s| {
-                    s.visit_expression(right)?;
-                    s.write(", ")?;
-                    s.visit_expression(left)
+                    s.write("NOT JSON_CONTAINS")?;
+                    s.surround_with("(", ")", |s| {
+                        s.visit_expression(right)?;
+                        s.write(", ")?;
+                        s.visit_expression(left)
+                    })
                 })
             } else {
                 self.visit_regular_difference_comparison(left, right)
@@ -750,6 +754,28 @@ mod tests {
 
         assert_eq!(
             "SELECT `User`.*, `Toto`.* FROM `User` LEFT JOIN `Post` AS `p` ON `p`.`userId` = `User`.`id`, `Toto`",
+            sql
+        );
+    }
+
+    #[test]
+    fn test_json_negation() {
+        let conditions = ConditionTree::not("json".equals(Value::Json(Some(serde_json::Value::Null))));
+        let (sql, _) = Mysql::build(Select::from_table("test").so_that(conditions)).unwrap();
+
+        assert_eq!(
+            "SELECT `test`.* FROM `test` WHERE (NOT (JSON_CONTAINS(`json`, ?) AND JSON_CONTAINS(?, `json`)))",
+            sql
+        );
+    }
+
+    #[test]
+    fn test_json_not_negation() {
+        let conditions = ConditionTree::not("json".not_equals(Value::Json(Some(serde_json::Value::Null))));
+        let (sql, _) = Mysql::build(Select::from_table("test").so_that(conditions)).unwrap();
+
+        assert_eq!(
+            "SELECT `test`.* FROM `test` WHERE (NOT (NOT JSON_CONTAINS(`json`, ?) OR NOT JSON_CONTAINS(?, `json`)))",
             sql
         );
     }
