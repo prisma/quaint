537300e30b06256a4fcd9a5822edc1ee9fe31d48
garren smith <garren.smith@gmail.com>
2021-12-23 16:08:25 +0200

    remove build features

--- src/visitor/postgres.rs
+++ src/visitor/postgres.rs
@@ -362,7 +362,6 @@ impl<'a> Visitor<'a> for Postgres<'a> {
         }
     }
 
-    #[cfg(feature = "postgresql")]
     fn visit_text_search(&mut self, text_search: crate::prelude::TextSearch<'a>) -> visitor::Result {
         let len = text_search.exprs.len();
         self.surround_with("to_tsvector(concat_ws(' ', ", "))", |s| {
@@ -378,7 +377,6 @@ impl<'a> Visitor<'a> for Postgres<'a> {
         })
     }
 
-    #[cfg(feature = "postgresql")]
     fn visit_matches(&mut self, left: Expression<'a>, right: std::borrow::Cow<'a, str>, not: bool) -> visitor::Result {
         if not {
             self.write("(NOT ")?;
@@ -395,7 +393,6 @@ impl<'a> Visitor<'a> for Postgres<'a> {
         Ok(())
     }
 
-    #[cfg(any(feature = "postgresql", feature = "mysql"))]
     fn visit_text_search_relevance(&mut self, text_search_relevance: TextSearchRelevance<'a>) -> visitor::Result {
         let len = text_search_relevance.exprs.len();
         let exprs = text_search_relevance.exprs;
