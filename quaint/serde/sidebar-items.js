8b93c1dd0fbb9cb73298993156f5989531785201
garrensmith <garrensmith@users.noreply.github.com>
2022-07-08 05:37:09 +0000

    Automated deployment: Fri Jul  8 05:37:09 UTC 2022 8f9d2c5fb1e0198777c556c4756bcdb28dc9859a

--- quaint/serde/sidebar-items.js
+++ quaint/serde/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["from_row","Deserialize a row into any type implementing `Deserialize`."],["from_rows","Deserialize each row of a `ResultSet`."]],"struct":[["ValueDeserializer",""]]});
+window.SIDEBAR_ITEMS = {"fn":[["from_row","Deserialize a row into any type implementing `Deserialize`."],["from_rows","Deserialize each row of a `ResultSet`."]],"struct":[["ValueDeserializer",""]]};
