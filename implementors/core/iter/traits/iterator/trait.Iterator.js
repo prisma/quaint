b4cc49dfc59e84ce9b0e6159610ec2bf32af08a3
cprieto <cprieto@users.noreply.github.com>
2022-08-11 15:16:24 +0000

    Automated deployment: Thu Aug 11 15:16:24 UTC 2022 fb4fe90682b4fecb485fd0d6975dd15a3bc9616b

--- implementors/core/iter/traits/iterator/trait.Iterator.js
+++ implementors/core/iter/traits/iterator/trait.Iterator.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["quaint"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"quaint/connector/struct.ResultSetIterator.html\" title=\"struct quaint::connector::ResultSetIterator\">ResultSetIterator</a>","synthetic":false,"types":["quaint::connector::result_set::ResultSetIterator"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"quaint":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"quaint/connector/struct.ResultSetIterator.html\" title=\"struct quaint::connector::ResultSetIterator\">ResultSetIterator</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
