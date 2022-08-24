89f2fb5edead73f833787e993373d1b84191bcc4
garrensmith <garrensmith@users.noreply.github.com>
2022-08-24 07:50:21 +0000

    Automated deployment: Wed Aug 24 07:50:21 UTC 2022 18d46a05237a363f847dd1d52c413d2c960d1d45

--- implementors/std/error/trait.Error.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"quaint":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"quaint/error/struct.Error.html\" title=\"struct quaint::error::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"quaint/error/enum.ErrorKind.html\" title=\"enum quaint::error::ErrorKind\">ErrorKind</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
