54c18adce875786f640fa06433e35cfa49a2593c
Tom Houlé <tom@tomhoule.com>
2022-11-15 08:11:41 +0100

    fix chrono failures in CI

--- src/connector/postgres/conversion.rs
+++ src/connector/postgres/conversion.rs
@@ -898,7 +898,7 @@ impl<'a> ToSql for Value<'a> {
                 (Value::Uuid(value), _) => value.map(|value| value.to_sql(ty, out)),
                 #[cfg(feature = "chrono")]
                 (Value::DateTime(value), &PostgresType::DATE) => {
-                    value.map(|value| value.date().naive_utc().to_sql(ty, out))
+                    value.map(|value| value.date_naive().to_sql(ty, out))
                 }
                 #[cfg(feature = "chrono")]
                 (Value::Date(value), _) => value.map(|value| value.to_sql(ty, out)),
